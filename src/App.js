e450e7772291e8ea15b8135842a2d923e10fb9b5
Asmita Kumari <asmitababhanlai@gmail.com>
2024-05-20 14:39:40 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Calculator = () => {
+  const [firstInput, setFirstInput] = useState("");
+  const [secondInput, setSecondInput] = useState("");
+
+  let firstNumber = parseInt(firstInput);
+  let secondNumber = parseInt(secondInput);
+
+  const validate = () => {
+    if (!firstInput || !secondInput) {
+      alert("Both numbers are required");
+      return false;
+    }
+    return true;
+  };
+
+  const add = (a, b) => a + b;
+  //   function add(a, b){return a + b}
+
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  const per = (a, b) => Math.round((b / a) * 100);
+
+  const callAdd = () => {
+    const check = validate();
+    if (check) alert(add(firstNumber, secondNumber));
+  };
+
+  const callSub = () => {
+    const check = validate();
+    if (check) alert(sub(firstNumber, secondNumber));
+  };
+
+  const callMul = () => {
+    const check = validate();
+    if (check) alert(mul(firstNumber, secondNumber));
+  };
+
+  const callDiv = () => {
+    const check = validate();
+    if (check) alert(div(firstNumber, secondNumber));
+  };
+
+  const callPer = () => {
+    const check = validate();
+    if (check) alert(per(firstNumber, secondNumber));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="number"
+        value={firstInput}
+        onChange={(f) => setFirstInput(f.target.value)}
+      />
+      <input
+        type="number"
+        value={secondInput}
+        onChange={(g) => setSecondInput(g.target.value)}
+      />
+      <button onClick={callAdd}> Addition </button>
+      <button onClick={callSub}> Subtract </button>
+      <button onClick={callMul}> Multiply </button>
+      <button onClick={callDiv}> Divide </button>
+      <button onClick={callPer}> Percent </button>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
